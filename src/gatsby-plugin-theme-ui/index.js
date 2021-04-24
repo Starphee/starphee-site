e1a29dc77b11fb830f3494c83aaa53bc28a6a1a4
Dorian Todd <dorianborian@gmail.com>
2021-04-24 12:49:49 -0700

    Fix site background

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,16 +1,15 @@
-import baseTheme from "gatsby-theme-musician/src/gatsby-plugin-theme-ui";
-
+import baseTheme from 'gatsby-theme-musician/src/gatsby-plugin-theme-ui';
 
 const style = {
   ...baseTheme,
 };
 
-style.styles.root.background = "rgb(53, 53, 53)";
-style.styles.root.color = "#fff";
-style.layout.header.background = "rgba(53,53,53,0.7)";
-style.layout.header.color = "#fff";
+style.styles.root.background = 'transparent';
+style.styles.root.color = '#fff';
+style.layout.header.background = '#44444482';
+style.layout.header.color = '#fff';
 
-style.textStyles.button.color = "#fff";
+style.textStyles.button.color = '#fff';
 
 // console.log(style);
 
