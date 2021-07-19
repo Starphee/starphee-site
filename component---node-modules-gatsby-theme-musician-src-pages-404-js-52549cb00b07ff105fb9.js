b67bad09e430d3beefd7a82ad6e6a6c1ae03347f
Starphee <Starphee@users.noreply.github.com>
2021-07-19 07:39:07 +0000

    deployed via Gatsby Publish Action 🎩 for a2bc7144eecb9692a113d151a51b368db83548da

---
+++ component---node-modules-gatsby-theme-musician-src-pages-404-js-52549cb00b07ff105fb9.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{JB8h:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),c=a("Wbzz"),u=a("ASLC"),o=a("aq6d"),r=a("D4+t");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(r.a,{pageTitle:"Page Not Found"}),l.a.createElement(u.b,null,l.a.createElement(u.c.h1,null,"Page Not Found :("),l.a.createElement(u.c.p,null,l.a.createElement(c.a,{to:"/"},"Return Home"))))}}}]);
+//# sourceMappingURL=component---node-modules-gatsby-theme-musician-src-pages-404-js-52549cb00b07ff105fb9.js.map
---
+++ sw.js
@@ -0,0 +1,113 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("workbox-v3.6.3/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
+
+workbox.core.setCacheNameDetails({prefix: "gatsby-plugin-offline"});
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "webpack-runtime-069b396a12768f8ae563.js"
+  },
+  {
+    "url": "framework-6c3ead24af170cdcb87c.js"
+  },
+  {
+    "url": "styles.64c4c0cdab4c8b884b85.css"
+  },
+  {
+    "url": "styles-31f023f48facb69035d3.js"
+  },
+  {
+    "url": "a9a7754c-47a4c00a55e0dc31ff43.js"
+  },
+  {
+    "url": "cb1608f2-4393e421a8066afd3afb.js"
+  },
+  {
+    "url": "app-02b245fecba88f3dfe12.js"
+  },
+  {
+    "url": "offline-plugin-app-shell-fallback/index.html",
+    "revision": "e085eec4124ad208bb353624f69b2c47"
+  },
+  {
+    "url": "component---node-modules-gatsby-theme-musician-node-modules-gatsby-plugin-offline-app-shell-js-4befadada98b1aead86f.js"
+  },
+  {
+    "url": "polyfill-1710eb3daa040b8ff904.js"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/(\.js$|\.css$|static\/)/, workbox.strategies.cacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\page-data\/.*\/page-data\.json/, workbox.strategies.networkFirst(), 'GET');
+workbox.routing.registerRoute(/^https?:.*\.(png|jpg|jpeg|webp|svg|gif|tiff|js|woff|woff2|json|css)$/, workbox.strategies.staleWhileRevalidate(), 'GET');
+workbox.routing.registerRoute(/^https?:\/\/fonts\.googleapis\.com\/css/, workbox.strategies.staleWhileRevalidate(), 'GET');
+
+/* global importScripts, workbox, idbKeyval */
+
+importScripts(`idb-keyval-iife.min.js`)
+
+const { NavigationRoute } = workbox.routing
+
+const navigationRoute = new NavigationRoute(async ({ event }) => {
+  let { pathname } = new URL(event.request.url)
+  pathname = pathname.replace(new RegExp(`^`), ``)
+
+  // Check for resources + the app bundle
+  // The latter may not exist if the SW is updating to a new version
+  const resources = await idbKeyval.get(`resources:${pathname}`)
+  if (!resources || !(await caches.match(`/app-02b245fecba88f3dfe12.js`))) {
+    return await fetch(event.request)
+  }
+
+  for (const resource of resources) {
+    // As soon as we detect a failed resource, fetch the entire page from
+    // network - that way we won't risk being in an inconsistent state with
+    // some parts of the page failing.
+    if (!(await caches.match(resource))) {
+      return await fetch(event.request)
+    }
+  }
+
+  const offlineShell = `/offline-plugin-app-shell-fallback/index.html`
+  return await caches.match(offlineShell)
+})
+
+workbox.routing.registerRoute(navigationRoute)
+
+const messageApi = {
+  setPathResources(event, { path, resources }) {
+    event.waitUntil(idbKeyval.set(`resources:${path}`, resources))
+  },
+
+  clearPathResources(event) {
+    event.waitUntil(idbKeyval.clear())
+  },
+}
+
+self.addEventListener(`message`, event => {
+  const { gatsbyApi } = event.data
+  if (gatsbyApi) messageApi[gatsbyApi](event, event.data)
+})
